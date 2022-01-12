0a18b93d8747c91fdc3848dea0226d2cfeac62d6
IoTeXNoob <95588237+IoTeXNoob@users.noreply.github.com>
2022-01-12 15:44:16 -0500

    Changed some bugs and variable names

--- liquidityLocker.sol
+++ liquidityLocker.sol
@@ -2,6 +2,11 @@
 
 pragma solidity ^0.8.0;
 
+/*
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+*/
 import "../OZ/IERC20.sol";
 
 import "../OZ/Ownable.sol";
@@ -14,17 +19,18 @@ address lpContract;
 
 uint256 lockDays;
 
-uint256 timeLocked = lockDays*17280; /* time locked will be calculated the following way 
+uint256 lockedBlocks; /* time locked will be calculated the following way 
  : lockDays is a number of days entered upon deployment of the contract,
    17280 comes from : (24hours * (60minutes per hour) * (60seconds per minute))/5seconds per block */
+
    /* timeLocked then gives us a block amount by multiplicating the number of desired days by the daily 
    number of blocks */ 
 
-address lpOwner = owner(); //defines the address
 
 constructor (uint256 _lockDays, address lpAddress){
     lockDays = _lockDays;
     lpContract = lpAddress;
+    lockedBlocks = (_lockDays*17280);
 }
 
 /* this is used for community members to validate the amount of days that the liquidity will be locked */
@@ -36,7 +42,7 @@ function getLockDays() external view returns(uint){
 /* this is used to get the time lock in block amount */
 
 function getLockBlocks() external view returns(uint){
-    return(timeLocked);
+    return(lockedBlocks);
 }
 
 /* only used to validate that we have stored the right contract address */
@@ -55,15 +61,15 @@ function setRefBlock() external onlyOwner{
 
 function calculateTimeLocked() internal view returns(bool){
     uint256 time = block.number;
-    return(time >= refBlock + timeLocked);
+    return(time >= refBlock + lockedBlocks);
 }
 
 /* allows us to withdraw LP tokens only when the timer ends */
 
 function withdrawLP () external onlyOwner {
-    require (calculateTimeLocked());
+    require (calculateTimeLocked(), "Trying to rugpull?!?");
     uint256 lpBalance = IERC20(lpContract).balanceOf(address(this));
-    IERC20(lpContract).transfer(lpOwner, lpBalance);
+    IERC20(lpContract).transfer(msg.sender, lpBalance);
 }
 
 }
